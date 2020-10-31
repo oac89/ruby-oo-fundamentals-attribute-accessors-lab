3770f7f9a0f98a39288c1c655539a236c62f2a16
oac89 <oacastro2@gmail.com>
2020-10-31 10:39:03 -0500

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,12 +1,14 @@
+require "pry"
 class Cat
     attr_accessor :cat, :name
+    
     def name
         self.name = "Maru"
     end
     def meow
         self.cat = puts "meow!"
     end
-    
+    binding.pry 
 
 end
 
